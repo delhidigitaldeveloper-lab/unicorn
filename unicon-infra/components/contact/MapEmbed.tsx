78f40a6769529dfd14c262054f7e4e322c1d7849
Delhi Digital Developer <delhidigitaldeveloper@gmail.com>
2026-07-19 18:48:23 +0530

    Update MapEmbed.tsx

--- unicon-infra/components/contact/MapEmbed.tsx
+++ unicon-infra/components/contact/MapEmbed.tsx
@@ -1,13 +1,12 @@
 export default function MapEmbed({
-  lat,
-  lng,
   label = "Unicon Infra",
 }: {
-  lat: number;
-  lng: number;
+  lat?: number;
+  lng?: number;
   label?: string;
 }) {
-  const src = `https://maps.app.goo.gl/eaFLGdPr6gDZdHCZ9`;
+  const src =
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4354.834286674461!2d72.51889109999999!3d23.051134100000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e9bc6ab22f6ff%3A0x6a9fba069a933bb9!2sUNICORN%20INFRA!5e1!3m2!1sen!2sin!4v1784466734629!5m2!1sen!2sin";
 
   return (
     <div className="relative h-72 md:h-96 rounded-2xl overflow-hidden gold-border">
